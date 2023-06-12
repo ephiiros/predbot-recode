852483954cb91ca3d4a15353b184e0a22c9e2379
ephiiros <52282201+ephiiros@users.noreply.github.com>
2023-06-13 00:57:38 +0200

    sd

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -33,71 +33,78 @@ module.exports = {
                             const today = DateTime.now() //.set({hour: 7}) //test
                             //const today = DateTime.fromSQL("2023-03-18 00:00:00")
 
-                            const todayGames = getDayGames(server.leagues, today)
-                            .then((response: loadGames[]) => {
-                                let todayString = ""
-                                let newResponse:loadGames[] = []
-                                response.forEach(game => {
-                                    if (game.DateTime_UTC < today) {
-                                        todayString += 
-                                        "~~" +
-                                        game.MatchId + " " + 
-                                        game.DateTime_UTC.toFormat("HH:mm") + " " +
-                                        game.Team1 + " vs " + game.Team2 + 
-                                        "~~\n" 
-                                    } else {
-                                        todayString += 
-                                        game.MatchId + " " + 
+                            if (server.leagues == null) {
+                                // error
+                                console.log("no leagues in server")
+                            } else {
+                                const todayGames = getDayGames(server.leagues, today)
+                                .then((response: loadGames[]) => {
+                                    let todayString = ""
+                                    let newResponse:loadGames[] = []
+                                    response.forEach(game => {
+                                        if (game.DateTime_UTC < today) {
+                                            todayString += 
+                                            "~~" +
+                                            game.MatchId + " " + 
+                                            game.DateTime_UTC.toFormat("HH:mm") + " " +
+                                            game.Team1 + " vs " + game.Team2 + 
+                                            "~~\n" 
+                                        } else {
+                                            todayString += 
+                                            game.MatchId + " " + 
+                                            game.DateTime_UTC.toFormat("HH:mm") + " " +
+                                            game.Team1 + " vs " + game.Team2 + "\n"
+                                            newResponse.push(game)
+                                        }
+                                    })
+                                    return [todayString, newResponse]
+                                })
+
+                                const tomorrowGames = getDayGames(server.leagues, today.plus({days:1}))
+                                .then((response: loadGames[]) => {
+                                    let tomorrowString = ""
+                                    response.forEach(game => {
+                                        tomorrowString +=
                                         game.DateTime_UTC.toFormat("HH:mm") + " " +
                                         game.Team1 + " vs " + game.Team2 + "\n"
-                                        newResponse.push(game)
+                                    })
+                                    return tomorrowString
+                                })
+
+                                const nextGame = getNextGame(server.leagues, today)
+                                .then((response: loadGames) => {
+                                    let nextGameString = ""
+                                    nextGameString += 
+                                    response.DateTime_UTC.toFormat("HH:mm") + " " + 
+                                    response.Team1 + " vs " + response.Team2 + "\n"
+
+                                    return nextGameString
+                                })
+
+                                Promise.all([todayGames, tomorrowGames, nextGame]).then((values) => {
+                                    let dailyMessage = 
+                                        "**CURRENT DATE AND TIME** " +
+                                        `${DateTime.now().toFormat("d LLLL y HH:mm ZZZZ")}\n` + 
+                                        "**GAMES TODAY**\n"
+
+                                    dailyMessage += values[0][0]
+                                    dailyMessage += "**GAMES TOMORROW**\n"
+                                    dailyMessage += values[1]
+                                    dailyMessage += "**NEXT GAME**\n"
+                                    dailyMessage += values[2]
+
+                                    // send for the day but lock individually
+
+                                    if (values[0][1].length > 0) {
+                                        setTimeout(sendVoteMessages, 2000, values[0][1], channel, today)
                                     }
+
+                                    channel.send(dailyMessage)
+
                                 })
-                                return [todayString, newResponse]
-                            })
 
-                            const tomorrowGames = getDayGames(server.leagues, today.plus({days:1}))
-                            .then((response: loadGames[]) => {
-                                let tomorrowString = ""
-                                response.forEach(game => {
-                                    tomorrowString +=
-                                    game.DateTime_UTC.toFormat("HH:mm") + " " +
-                                    game.Team1 + " vs " + game.Team2 + "\n"
-                                })
-                                return tomorrowString
-                            })
+                            }
 
-                            const nextGame = getNextGame(server.leagues, today)
-                            .then((response: loadGames) => {
-                                let nextGameString = ""
-                                nextGameString += 
-                                response.DateTime_UTC.toFormat("HH:mm") + " " + 
-                                response.Team1 + " vs " + response.Team2 + "\n"
-
-                                return nextGameString
-                            })
-
-                            Promise.all([todayGames, tomorrowGames, nextGame]).then((values) => {
-                                let dailyMessage = 
-                                    "**CURRENT DATE AND TIME** " +
-                                    `${DateTime.now().toFormat("d LLLL y HH:mm ZZZZ")}\n` + 
-                                    "**GAMES TODAY**\n"
-
-                                dailyMessage += values[0][0]
-                                dailyMessage += "**GAMES TOMORROW**\n"
-                                dailyMessage += values[1]
-                                dailyMessage += "**NEXT GAME**\n"
-                                dailyMessage += values[2]
-
-                                // send for the day but lock individually
-
-                                if (values[0][1].length > 0) {
-                                    setTimeout(sendVoteMessages, 2000, values[0][1], channel, today)
-                                }
-
-                                channel.send(dailyMessage)
-
-                            })
                         }
                     })
                 })
