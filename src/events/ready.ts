404f61d586600188e87512f5d7c58def23f0385a
ephiiros <52282201+ephiiros@users.noreply.github.com>
2023-06-17 17:08:03 +0200

    IDK

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -21,6 +21,86 @@ module.exports = {
             return servers
         }).then(() => {
             console.log("[" + DateTime.now().toFormat("HH:mm") + "] [Server] Creating daily schedule")
+                getServers().then((servers) => {
+                    servers.forEach((server) => {
+                        if (server.channel != null) {
+                            console.log("["+ server.id +"] Scheduled Message")
+                            const channel = client.channels.cache.get(server.channel) as TextChannel
+                            const today = DateTime.now() //.set({hour: 8, minute: 58}) //test
+                            //const today = DateTime.fromSQL("2023-03-18 00:00:00")
+
+                            if (server.leagues == null) {
+                                // error
+                                console.log("[" + server.id + "] no leagues in server")
+                            } else {
+                                const todayGames = getDayGames(server.leagues, today)
+                                .then((response: loadGames[]) => {
+                                    let todayString = ""
+                                    let newResponse:loadGames[] = []
+                                    response.forEach(game => {
+                                        if (game.DateTime_UTC < today) {
+                                            todayString += 
+                                            "~~" +
+                                            game.DateTime_UTC.toFormat("HH:mm") + " " +
+                                            game.Team1 + " vs " + game.Team2 + 
+                                            "~~\n" 
+                                        } else {
+                                            todayString += 
+                                            game.DateTime_UTC.toFormat("HH:mm") + " " +
+                                            game.Team1 + " vs " + game.Team2 + "\n"
+                                            newResponse.push(game)
+                                        }
+                                    })
+                                    return [todayString, newResponse]
+                                })
+
+                                const tomorrowGames = getDayGames(server.leagues, today.plus({days:1}))
+                                .then((response: loadGames[]) => {
+                                    let tomorrowString = ""
+                                    response.forEach(game => {
+                                        tomorrowString +=
+                                        game.DateTime_UTC.toFormat("HH:mm") + "  " +
+                                        game.Team1 + " vs " + game.Team2 + "\n"
+                                    })
+                                    return tomorrowString
+                                })
+
+                                const nextGame = getNextGame(server.leagues, today)
+                                .then((response: loadGames) => {
+                                    let nextGameString = ""
+                                    nextGameString += 
+                                    response.DateTime_UTC.toFormat("HH:mm") + " " + 
+                                    response.Team1 + " vs " + response.Team2 + "\n"
+
+                                    return nextGameString
+                                })
+
+                                Promise.all([todayGames, tomorrowGames, nextGame]).then((values) => {
+                                    let dailyMessage = 
+                                        "**CURRENT DATE AND TIME** " +
+                                        `${today.toFormat("d LLLL y HH:mm ZZZZ")}\n` + 
+                                        "**GAMES TODAY**\n"
+
+                                    dailyMessage += values[0][0]
+                                    dailyMessage += "**GAMES TOMORROW**\n"
+                                    dailyMessage += values[1]
+                                    dailyMessage += "**NEXT GAME**\n"
+                                    dailyMessage += values[2]
+
+                                    // send for the day but lock individually
+
+                                    if (values[0][1].length > 0) {
+                                        setTimeout(sendVoteMessages, 2000, values[0][1], channel, today)
+                                    }
+
+                                    channel.send(dailyMessage)
+
+                                })
+                        }
+                    }}
+                    )
+                })
+
             // every 24 hours 0 0 * * * 
             // every minute * * * * * 
             cron.schedule('0 0 * * * ', () => {
