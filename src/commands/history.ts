270fda52694ceab06840f0bb8847b3f4966d9452
ephiros <iliakoskos@gmail.com>
2023-08-12 16:53:22 +0200

    s

--- src/commands/history.ts
+++ src/commands/history.ts
@@ -1,55 +1,39 @@
 import { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } from "discord.js";
-//import { getHistory } from "../libs/mongoWrapper";
+import { getHistory } from "../libs/mongoWrapper";
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('history')
 		.setDescription('history'),
 	async execute(interaction:any) {
-        //const history = await getHistory(interaction.member.id)
-        //@ts-ignore
-        //let interactionString = '\n'
-        //let page = 0
-        //for (let i = page*5; i < page*5 + 5; i ++){
-        //    interactionString += history[i].matchId +"\n"
-        //}
+        const history = await getHistory(interaction.member.id)
 
-		const confirm = new ButtonBuilder()
-			.setCustomId('confirm')
-			.setLabel('Confirm Ban')
-			.setStyle(ButtonStyle.Danger);
+        let interactionString = '\n'
 
-		const cancel = new ButtonBuilder()
-			.setCustomId('cancel')
-			.setLabel('Cancel')
-			.setStyle(ButtonStyle.Secondary);
+        let page = 0
 
-		const row = new ActionRowBuilder()
-			.addComponents(cancel, confirm);
+        for (let i = page*5; i < page*5 + 5; i ++) {
+            interactionString += history[i].matchId +"\n"
+        }
 
-		await interaction.reply({
-            content: "test",
-            componenets: [row]
+        const left = new ButtonBuilder()
+            .setCustomId('left')
+            .setLabel('<')
+            .setStyle(ButtonStyle.Primary)
+
+        const right = new ButtonBuilder()
+            .setCustomId('right')
+            .setLabel('>')
+            .setStyle(ButtonStyle.Primary)
+
+        const row = new ActionRowBuilder()
+            .addComponents(left, right)
+
+		const response = await interaction.reply({
+            content: interactionString,
+            components: [row]
         });
 
-	},
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*
         //@ts-ignore
         const collectorFilter = i => i.user.id === interaction.user.id;
 
@@ -79,11 +63,13 @@ module.exports = {
                         content: interactionString, 
                         components: [row] 
                     });
-	}
+            }
+
         } catch (e) {
             await interaction.editReply({ 
-                content: 'Confirmation not received within 1 minute, cancelling', 
+                content: 'Timed out', 
                 components: [] 
             });
         }
-    */
+	},
+}
